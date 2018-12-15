04ea50921a18f57fcfb607f38120ec27661e2067
Jean Lestang <jelestang@airfrance.fr>
2018-12-15 06:26:17 +0100

    Extraction des catalogues summer 2018

--- Catalogues/extraitProduitsCatalogue.sh
+++ Catalogues/extraitProduitsCatalogue.sh
@@ -7,35 +7,17 @@ echo "Paramètre : $1 "
 echo $0
 repertoireOrigine=`pwd`
 
-echo "Repoertoire source :  $repertoireOrigine"
-
 DIRNAME="$( dirname "$1" )"
 FNAME="${1##*/}"
-echo "repe :  $DIRNAME"
-echo "fichier :  ${1##*/}"
-
 fichierSorti=${FNAME/txt/csv}
-echo "Fichier sorti : $fichierSorti"
 
-echo "On y va !"
 cd "$DIRNAME"
-pwd
 
-
-echo
-echo 
-
+echo " '$FNAME' >> '$fichierSorti'"
 ls -la "$FNAME"
-echo
-
-echo "exécution de l'extraction"
-echo "awk -f ../extractionCodesProduits.awk \"$FNAME\" > \"$fichierSorti\""
 awk -f ../extractionCodesProduits.awk "$FNAME" > "$fichierSorti"
 ls -la "$fichierSorti"
 
-echo "On retourne à la maison"
 cd "$repertoireOrigine"
-pwd
 
-echo
 echo
