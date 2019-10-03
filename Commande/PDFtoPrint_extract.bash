9226bbb7d393474fee3dc8ee63902e3378b1cd01
Jean Lestang <jelestang@airfrance.fr>
2019-10-03 20:58:19 +0200

    Commande Fall 2019

--- Commande/PDFtoPrint_extract.bash
+++ Commande/PDFtoPrint_extract.bash
@@ -8,9 +8,11 @@ java -jar /c/Users/m312742/Documents/Perso/obg/PDFBox/pdfbox-app-2.0.13.jar Prin
 start excel ../Catalogues/spring\ 2019\ presentation/catalogo\ capsule\ matelasseÌe\ 19.PRODUIT.csv
 
 
-# 1_spring 2020/spring 2020.txt; 1 item(s) -> 1_spring\ 2020/spring\ 2020.pdf
+# 1_spring 2020/spring 2020.txt; 2 item(s) -> 1_spring\ 2020/spring\ 2020.pdf
 java -jar /c/Users/m312742/Documents/Perso/obg/PDFBox/pdfbox-app-2.0.13.jar PDFSplit -startPage 60 -endPage 60 -outputPrefix PDFextract/extrait1 ../Catalogues/1_spring\ 2020/spring\ 2020.pdf
-mv PDFextract/extrait-1.pdf PDFextract/1_spring\ 2020/spring\ 2020.pdf
+java -jar /c/Users/m312742/Documents/Perso/obg/PDFBox/pdfbox-app-2.0.13.jar PDFSplit -startPage 111 -endPage 111 -outputPrefix PDFextract/extrait2 ../Catalogues/1_spring\ 2020/spring\ 2020.pdf
+java -jar /c/Users/m312742/Documents/Perso/obg/PDFBox/pdfbox-app-2.0.13.jar PDFMerger PDFextract/extrait*.pdf PDFextract/1_spring\ 2020/spring\ 2020.pdf
+rm PDFextract/extrait*.pdf
 echo; ls -la PDFextract/1_spring\ 2020/spring\ 2020.pdf
 
 java -jar /c/Users/m312742/Documents/Perso/obg/PDFBox/pdfbox-app-2.0.13.jar PrintPDF PDFextract/1_spring\ 2020/spring\ 2020.pdf
@@ -58,6 +60,16 @@ java -jar /c/Users/m312742/Documents/Perso/obg/PDFBox/pdfbox-app-2.0.13.jar Prin
 start excel ../Catalogues/spring\ 2019\ presentation/catalogo\ Spring\ 19\ continuativo.PRODUIT.csv
 
 
+# spring 2018 presentation/catalogo O bag for Disney 2018.txt; 1 item(s) -> catalogo\ O\ bag\ for\ Disney\ 2018.pdf
+java -jar /c/Users/m312742/Documents/Perso/obg/PDFBox/pdfbox-app-2.0.13.jar PDFSplit -startPage 11 -endPage 11 -outputPrefix PDFextract/extrait1 ../Catalogues/spring\ 2018\ presentation/catalogo\ O\ bag\ for\ Disney\ 2018.pdf
+mv PDFextract/extrait-1.pdf PDFextract/catalogo\ O\ bag\ for\ Disney\ 2018.pdf
+echo; ls -la PDFextract/catalogo\ O\ bag\ for\ Disney\ 2018.pdf
+
+java -jar /c/Users/m312742/Documents/Perso/obg/PDFBox/pdfbox-app-2.0.13.jar PrintPDF PDFextract/catalogo\ O\ bag\ for\ Disney\ 2018.pdf
+
+start excel ../Catalogues/spring\ 2018\ presentation/catalogo\ O\ bag\ for\ Disney\ 2018.PRODUIT.csv
+
+
 # spring 2019 presentation/catalogo Continuativo_Spring_19.txt; 1 item(s) -> catalogo\ Continuativo_Spring_19.pdf
 java -jar /c/Users/m312742/Documents/Perso/obg/PDFBox/pdfbox-app-2.0.13.jar PDFSplit -startPage 34 -endPage 34 -outputPrefix PDFextract/extrait1 ../Catalogues/spring\ 2019\ presentation/catalogo\ Continuativo_Spring_19.pdf
 mv PDFextract/extrait-1.pdf PDFextract/catalogo\ Continuativo_Spring_19.pdf
@@ -68,8 +80,8 @@ java -jar /c/Users/m312742/Documents/Perso/obg/PDFBox/pdfbox-app-2.0.13.jar Prin
 start excel ../Catalogues/spring\ 2019\ presentation/catalogo\ Continuativo_Spring_19.PRODUIT.csv
 
 
-# winter 2018 presentation/catalogo winter 18.txt; 4 item(s) -> catalogo\ winter\ 18.pdf
-java -jar /c/Users/m312742/Documents/Perso/obg/PDFBox/pdfbox-app-2.0.13.jar PDFSplit -startPage 99 -endPage 99 -outputPrefix PDFextract/extrait1 ../Catalogues/winter\ 2018\ presentation/catalogo\ winter\ 18.pdf
+# winter 2018 presentation/catalogo winter 18.txt; 2 item(s) -> catalogo\ winter\ 18.pdf
+java -jar /c/Users/m312742/Documents/Perso/obg/PDFBox/pdfbox-app-2.0.13.jar PDFSplit -startPage 42 -endPage 42 -outputPrefix PDFextract/extrait1 ../Catalogues/winter\ 2018\ presentation/catalogo\ winter\ 18.pdf
 mv PDFextract/extrait-1.pdf PDFextract/catalogo\ winter\ 18.pdf
 echo; ls -la PDFextract/catalogo\ winter\ 18.pdf
 
@@ -88,7 +100,7 @@ java -jar /c/Users/m312742/Documents/Perso/obg/PDFBox/pdfbox-app-2.0.13.jar Prin
 start excel ../Catalogues/fall\ 2018\ presentation/catalogo\ 2018\ continuativo.PRODUIT.csv
 
 
-# Inconnu; 147 item(s) -> Inconnu
+# Inconnu; 231 item(s) -> Inconnu
 java -jar /c/Users/m312742/Documents/Perso/obg/PDFBox/pdfbox-app-2.0.13.jar PDFSplit -startPage 2 -endPage 2 -outputPrefix PDFextract/extrait1 ../Catalogues/Inconnu
 mv PDFextract/extrait-1.pdf PDFextract/Inconnu
 echo; ls -la PDFextract/Inconnu
@@ -128,11 +140,21 @@ java -jar /c/Users/m312742/Documents/Perso/obg/PDFBox/pdfbox-app-2.0.13.jar Prin
 start excel ../Catalogues/summer\ 2018\ presentation/catalogo\ summer\ 2018\ continuativo.PRODUIT.csv
 
 
+# fall 2018 presentation/catalogo fall 2018.txt; 2 item(s) -> catalogo\ fall\ 2018.pdf
+java -jar /c/Users/m312742/Documents/Perso/obg/PDFBox/pdfbox-app-2.0.13.jar PDFSplit -startPage 39 -endPage 39 -outputPrefix PDFextract/extrait1 ../Catalogues/fall\ 2018\ presentation/catalogo\ fall\ 2018.pdf
+mv PDFextract/extrait-1.pdf PDFextract/catalogo\ fall\ 2018.pdf
+echo; ls -la PDFextract/catalogo\ fall\ 2018.pdf
+
+java -jar /c/Users/m312742/Documents/Perso/obg/PDFBox/pdfbox-app-2.0.13.jar PrintPDF PDFextract/catalogo\ fall\ 2018.pdf
+
+start excel ../Catalogues/fall\ 2018\ presentation/catalogo\ fall\ 2018.PRODUIT.csv
+
+
 # fall 2019 presentation_/catalogo fall 19.txt; 6 item(s) -> catalogo\ fall\ 19.pdf
-java -jar /c/Users/m312742/Documents/Perso/obg/PDFBox/pdfbox-app-2.0.13.jar PDFSplit -startPage 88 -endPage 88 -outputPrefix PDFextract/extrait1 ../Catalogues/fall\ 2019\ presentation_/catalogo\ fall\ 19.pdf
-java -jar /c/Users/m312742/Documents/Perso/obg/PDFBox/pdfbox-app-2.0.13.jar PDFSplit -startPage 89 -endPage 89 -outputPrefix PDFextract/extrait2 ../Catalogues/fall\ 2019\ presentation_/catalogo\ fall\ 19.pdf
+java -jar /c/Users/m312742/Documents/Perso/obg/PDFBox/pdfbox-app-2.0.13.jar PDFSplit -startPage 46 -endPage 46 -outputPrefix PDFextract/extrait1 ../Catalogues/fall\ 2019\ presentation_/catalogo\ fall\ 19.pdf
+java -jar /c/Users/m312742/Documents/Perso/obg/PDFBox/pdfbox-app-2.0.13.jar PDFSplit -startPage 55 -endPage 55 -outputPrefix PDFextract/extrait2 ../Catalogues/fall\ 2019\ presentation_/catalogo\ fall\ 19.pdf
 java -jar /c/Users/m312742/Documents/Perso/obg/PDFBox/pdfbox-app-2.0.13.jar PDFSplit -startPage 91 -endPage 91 -outputPrefix PDFextract/extrait3 ../Catalogues/fall\ 2019\ presentation_/catalogo\ fall\ 19.pdf
-java -jar /c/Users/m312742/Documents/Perso/obg/PDFBox/pdfbox-app-2.0.13.jar PDFSplit -startPage 103 -endPage 103 -outputPrefix PDFextract/extrait4 ../Catalogues/fall\ 2019\ presentation_/catalogo\ fall\ 19.pdf
+java -jar /c/Users/m312742/Documents/Perso/obg/PDFBox/pdfbox-app-2.0.13.jar PDFSplit -startPage 143 -endPage 143 -outputPrefix PDFextract/extrait4 ../Catalogues/fall\ 2019\ presentation_/catalogo\ fall\ 19.pdf
 java -jar /c/Users/m312742/Documents/Perso/obg/PDFBox/pdfbox-app-2.0.13.jar PDFMerger PDFextract/extrait*.pdf PDFextract/catalogo\ fall\ 19.pdf
 rm PDFextract/extrait*.pdf
 echo; ls -la PDFextract/catalogo\ fall\ 19.pdf
